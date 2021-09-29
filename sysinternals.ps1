d949de1a0fd9ef0aa078b1b5871eb70855508ca0
Marcus Otto <89423637+mco9734@users.noreply.github.com>
2021-09-28 20:56:40 -0400

    Add files via upload

---
+++ sysinternals.ps1
@@ -0,0 +1,23 @@
+Invoke-WebRequest "https://live.sysinternals.com/Autoruns.exe" -OutFile "nothing.exe"
+Invoke-WebRequest "https://live.sysinternals.com/Sysmon.exe" -Outfile "garbage.exe"
+Invoke-WebRequest "https://live.sysinternals.com/Procmon.exe" -Outfile "null.exe"
+Invoke-WebRequest "https://live.sysinternals.com/procexp.exe" -Outfile "epic.exe"
+
+Invoke-WebRequest https://github.com/olafhartong/sysmon-modular/archive/refs/heads/master.zip -OutFile "sysmon-modular-master.zip"
+Expand-Archive ./"sysmon-modular-master.zip"
+Remove-Item ./"sysmon-modular-master.zip"
+Set-Location .\sysmon-modular-master
+Set-Location .\sysmon-modular-master
+. .\Merge-SysmonXml.ps1
+Merge-AllSysmonXml -Path ( Get-ChildItem '[0-9]*\*.xml') -AsString | Out-File sysmonconfig.xml
+Set-Location ..
+Set-Location ..
+Move-Item -Path .\sysmon-modular-master\sysmon-modular-master\sysmonconfig.xml -Destination .\
+
+sysmon.exe -i sysmonconfig.xml -accepteula
+
+eventvwr.msc
+
+
+
+
