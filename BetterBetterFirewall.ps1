8f94d139b920e317f1f4707a472d22a31f49dd3f
Marcus Otto <89423637+mco9734@users.noreply.github.com>
2021-10-15 23:26:31 -0400

    UB Lockdown POG

---
+++ BetterBetterFirewall.ps1
@@ -0,0 +1,30 @@
+netsh advfirewall reset
+netsh advfirewall set allprofiles state on
+netsh advfirewall firewall delete rule name=all
+netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
+
+# ICMP
+netsh advfirewall firewall add rule name="ICMP" dir=in action=allow protocol=icmpv4:8,any
+netsh advfirewall firewall add rule name="ICMP" dir=out action=allow protocol=icmpv4:8,any
+
+# DNS
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp remoteport=53 dir=out 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp localport=53 dir=out 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp remoteport=53 dir=in 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp localport=53 dir=in 
+
+# AD Services
+netsh advfirewall firewall add rule name="AD1-tcp-in" action=allow protocol=tcp remoteport=389,445,88,135,3268 dir=in remoteip=10.5.1.60
+netsh advfirewall firewall add rule name="AD1-udp-in" action=allow protocol=udp remoteport=389,445,88,135,123 dir=in remoteip=10.5.1.60
+netsh advfirewall firewall add rule name="AD1-tcp-out" action=allow protocol=tcp remoteport=389,445,88,135,3268 dir=out remoteip=10.5.1.60
+netsh advfirewall firewall add rule name="AD1-udp-out" action=allow protocol=udp remoteport=389,445,88,135,123 dir=out remoteip=10.5.1.60
+
+# WinRM
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp localport=5985,5986 action=allow 
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-out)" dir=out protocol=tcp localport=5985,5986 action=allow 
+
+netsh advfirewall set allprofiles logging filename $HOME\Desktop\fw.log
+netsh advfirewall set allprofiles logging maxfilesize 32676
+netsh advfirewall set allprofiles logging droppedconnections enable
+netsh advfirewall set allprofiles logging allowedconnections enable
+netsh advfirewall set allprofiles settings inboundusernotification enable
