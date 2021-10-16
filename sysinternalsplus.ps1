f9bcb2110444c96fa09200f4f8222e8987b674b2
Marcus Otto <89423637+mco9734@users.noreply.github.com>
2021-10-15 23:35:33 -0400

    big sneak

--- sysinternalsplus.ps1
+++ sysinternalsplus.ps1
@@ -1,11 +1,11 @@
-Set-Location $HOME\Pictures
+Set-Location $HOME\Music
 
-Invoke-WebRequest -o .\ar.zip "https://download.sysinternals.com/files/Autoruns.zip"
-Invoke-WebRequest -o .\tv.zip "https://download.sysinternals.com/files/TCPView.zip"
-Invoke-WebRequest -o .\sm.zip "https://download.sysinternals.com/files/Sysmon.zip"
-Invoke-WebRequest -o .\pe.zip "https://download.sysinternals.com/files/ProcessExplorer.zip"
-Invoke-WebRequest -o .\pm.zip "https://download.sysinternals.com/files/ProcessMonitor.zip"
+Invoke-WebRequest -o .\autor.zip "https://download.sysinternals.com/files/Autoruns.zip"
+Invoke-WebRequest -o .\tvw.zip "https://download.sysinternals.com/files/TCPView.zip"
+Invoke-WebRequest -o .\symo.zip "https://download.sysinternals.com/files/Sysmon.zip"
+Invoke-WebRequest -o .\pexp.zip "https://download.sysinternals.com/files/ProcessExplorer.zip"
+Invoke-WebRequest -o .\prmo.zip "https://download.sysinternals.com/files/ProcessMonitor.zip"
 
-Invoke-WebRequest -o .\gawr.exe “https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe”
+Invoke-WebRequest -o .\ws.exe “https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe”
 #Invoke-WebRequest -o .\plus.zip “https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”
-Invoke-WebRequest -o .\webint.exe “http://dl.google.com/chrome/install/375.126/chrome_installer.exe”
+Invoke-WebRequest -o .\gamer.exe “http://dl.google.com/chrome/install/375.126/chrome_installer.exe”
