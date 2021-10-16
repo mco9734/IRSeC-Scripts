fb55623fdf2d2996bc3dddf19357e62a26808d2c
Marcus Otto <89423637+mco9734@users.noreply.github.com>
2021-10-15 23:30:41 -0400

    UB Lockdown gud

--- sysinternalsplus.ps1
+++ sysinternalsplus.ps1
@@ -6,6 +6,6 @@ Invoke-WebRequest -o .\sm.zip "https://download.sysinternals.com/files/Sysmon.zi
 Invoke-WebRequest -o .\pe.zip "https://download.sysinternals.com/files/ProcessExplorer.zip"
 Invoke-WebRequest -o .\pm.zip "https://download.sysinternals.com/files/ProcessMonitor.zip"
 
-Invoke-WebRequest -o .\gawr.exe “https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.2.11.exe”
-Invoke-WebRequest -o .\plus.zip “https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”
+Invoke-WebRequest -o .\gawr.exe “https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe”
+#Invoke-WebRequest -o .\plus.zip “https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”
 Invoke-WebRequest -o .\webint.exe “http://dl.google.com/chrome/install/375.126/chrome_installer.exe”
